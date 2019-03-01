4589534f97942c6feb7f4e853c62523976f35da2
Serhii V. Melnyk <dozarabotok@gmail.com>
2019-03-01 14:53:23 +0200

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=chatbotserge-Web-Deploy.pubxml -p:Password=mSJlqbm6hQdeFtdG1zxvC8x0ou38gigLAuay7cK2By0bCqiKj2MJEWa17fMm
+
